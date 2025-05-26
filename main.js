6dbc603d51531c1894b2ac7637fdca96034bdc94
Alejandro Mühle Diaz <jicotea11@gmail.com>
2025-05-26 13:51:33 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+
+
+
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const loader = new THREE.TextureLoader();
+loader.load('premium_photo-1680582107403-04dfac02efc3.jpg', function(texture) {
+  scene.background = texture;
+});
+
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('https://media.istockphoto.com/id/1640602841/de/foto/textur-der-wand-aus-knorrig-geschliffenen-holzbrettern-als-retro-naturhintergrund.webp?a=1&b=1&s=612x612&w=0&k=20&c=GGkK5hnax4rFgSfCsFvenfR6QgeTlbbo6Y8X2Pobz8o=');
+
+
+const material = new THREE.MeshBasicMaterial({ map: texture });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
+camera.position.z = 2;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.007;
+  cube.rotation.y += 0.007;
+  renderer.render(scene, camera);
+}
+animate();
